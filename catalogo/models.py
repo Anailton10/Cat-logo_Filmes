e61c569a1f20965514507d2649d6ea4d2184754d
Anailton Silva <francisco.anailton17@gmail.com>
2023-10-22 19:29:03 -0300

    adicionando area de login e cadastro de adm

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,11 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+
+class Adm(models.Model):
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    senha = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.nome
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Adm
+from django.urls import reverse
+from hashlib import sha256
+from django.contrib.auth import authenticate, login, logout
 
 
 def cadastro_admin(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
     elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        conf_senha = request.POST.get('conf_senha')
+        # TODO: Fazer os tratamentos de erros junto com suas mensagens
+        if Adm.objects.filter(email=email).exists():
+            return HttpResponse('Usuario já existe')
+        else:
+            if senha != conf_senha:
+                return HttpResponse('Senhas não são iguais')
+
+        senha = sha256(senha.encode()).hexdigest()
+        novo_usuario = Adm(nome=nome,
+                           email=email,
+                           senha=senha)
+        novo_usuario.save()
+        usuario = authenticate(request, username=email, password=senha)
+        if usuario:
+            login(request, usuario)
+    return HttpResponse('Cadastrado')
+
+
+def login_admin(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            # TODO: redirecionar para arena de add os filmes
+            return render(request, 'teste.html')
+    return render(request, 'login.html')
+
+
+def logout_admin(request):
+    logout(request)
+    return redirect(reverse('login_admin'))

e118f9e69ea6fd0e5cf68308773701c2f9def03c
Anailton Silva <francisco.anailton17@gmail.com>
2023-10-25 20:24:42 -0300

    criando models para cadastrar os filmes

--- filmes/models.py
+++ filmes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Cadastro_Filmes(models.Model):
+    titulo = models.CharField(max_length=50)
+    sinopse = models.TextField()
+    capa = models.FileField(upload_to='capa')
+    duracao = models.IntegerField()
+    autor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.titulo

49fe41c085a5cd9ef9226368f3f804250505114f
Anailton Silva <francisco.anailton17@gmail.com>
2023-10-31 19:18:41 -0300

    Adicionando mensagens para melhor interação

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Adm
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.urls import reverse
 from hashlib import sha256
 
@@ -13,18 +15,26 @@ def cadastro_admin(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         conf_senha = request.POST.get('conf_senha')
-        # TODO: Fazer os tratamentos de erros junto com suas mensagens
+
         if Adm.objects.filter(email=email).exists():
-            return HttpResponse('Usuario já existe')
+            messages.add_message(request, constants.ERROR, 'Usuario já existe')
+            return redirect(reverse(cadastro_admin))
         else:
             if senha != conf_senha:
-                return HttpResponse('Senhas não são iguais')
-
+                messages.add_message(
+                    request, constants.ERROR, 'Senhas não são iguais')
+                return redirect(reverse(cadastro_admin))
+            if len(senha) <= 6:
+                messages.add_message(
+                    request, constants.ERROR, 'Senha curta, informe uma senha com 7 digitos')
+                return redirect(reverse(cadastro_admin))
         senha = sha256(senha.encode()).hexdigest()
         novo_usuario = Adm(nome=nome,
                            email=email,
                            senha=senha)
         novo_usuario.save()
+        messages.add_message(request, constants.SUCCESS,
+                             'Conta cadastrada com sucesso')
     return redirect(reverse('login_admin'))
 
 
@@ -32,17 +42,17 @@ def login_admin(request):
     if request.method == "GET":
         return render(request, 'login.html')
     elif request.method == "POST":
-        nome = request.POST.get('nome')
+        email = request.POST.get('email')
         senha = request.POST.get('senha')
         senha = sha256(senha.encode()).hexdigest()
-        adm = Adm.objects.filter(nome=nome).filter(senha=senha)
+        adm = Adm.objects.filter(email=email).filter(senha=senha)
         if len(adm) == 0:
-            return HttpResponse('Usuario não cadastrado.')
+            messages.add_message(request, constants.ERROR,
+                                 'Usuario não existe')
+            return redirect(reverse('login_admin'))
         elif len(adm) > 0:
             request.session['adm'] = adm[0].id
             return redirect(reverse('adicionar_filmes'))
-
-# TODO: adicionar mensagens para melhor interação
 
 
 def logout_admin(request):
--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from django.contrib.messages import constants
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
